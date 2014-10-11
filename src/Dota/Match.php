f1999cfebc0faf561ee4c53cb81f22f9f67bcc04
phpislove <ilya.s.coding@gmail.com>
2014-10-11 16:26:31 +0700

    Match now can grab data from DotA API server

--- src/Dota/Match.php
+++ src/Dota/Match.php
@@ -1,11 +1,24 @@
 <?php namespace Dota;
 
+use Dota\WebApi\Client;
+
 class Match {
+
+    /**
+     * @var string
+     */
+    const URL =
+        'https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001';
 
     /**
      * @var integer
      */
     protected $id;
+
+    /**
+     * @var array
+     */
+    protected $data = [];
 
     /**
      * @param integer $id
@@ -14,6 +27,18 @@ class Match {
     public function __construct($id)
     {
         $this->id = $id;
+    }
+
+    /**
+     * @param Client $client
+     * @return void
+     */
+    public function loadData(Client $client)
+    {
+        if ( ! count($this->data))
+        {
+            $this->data = $client->get(static::URL, ['match_id' => $this->id]);
+        }
     }
 
     /**
